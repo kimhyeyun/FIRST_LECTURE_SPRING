9d1c22684c6e221e5dc8b0b44a3f0a7412b97b65
Kimhyeyun <cqqwer@gmail.com>
2022-12-30 19:19:27 +0900

    Feat: 손님이 주문하는 기능 추가

--- src/test/java/org/example/ch3/order/CustomerTest.java
+++ src/test/java/org/example/ch3/order/CustomerTest.java
@@ -21,5 +21,23 @@ package org.example.ch3.order;
 * 6. 구현하기
 * */
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThatCode;
+
 public class CustomerTest {
+
+    @DisplayName("메뉴이름에 해당하는 요리를 주문한다.")
+    @Test
+    void orderTest() {
+        Customer customer = new Customer();
+        Menu menu = new Menu(List.of(new MenuItem("돈가스", 5000), new MenuItem("냉면", 7000),new MenuItem("만두", 3000)));
+        Cooking cooking = new Cooking();
+
+        assertThatCode(() -> customer.order("만두", menu, cooking))
+                .doesNotThrowAnyException();
+    }
 }
