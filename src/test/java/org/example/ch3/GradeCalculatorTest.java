e2aab0fff5b773a84ed7fdd27425043ccf51bfc7
Kimhyeyun <cqqwer@gmail.com>
2022-12-30 18:43:05 +0900

    Test: 학점 계산에 대한 테스트코드 먼저 작성

--- src/test/java/org/example/ch3/GradeCalculatorTest.java
+++ src/test/java/org/example/ch3/GradeCalculatorTest.java
@@ -3,6 +3,10 @@ package org.example.ch3;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 /**
  * 요구사항
  * 평균학점 계산 방법 = (학점수×교과목 평점)의 합계/수강신청 총학점 수
@@ -20,6 +24,12 @@ public class GradeCalculatorTest {
 
     @Test
     void calucateGradeTest() {
+        List<Course> courses = List.of(new Course("OOP", 3, "A"),
+                new Course("자료구조", 3, "A"));
 
+        GradeCalculator gradeCalculator = new GradeCalculator(courses);
+        double gradeResult = gradeCalculator.calculateGrade();
+
+        assertThat(gradeResult).isEqualTo(4.5);
     }
 }
