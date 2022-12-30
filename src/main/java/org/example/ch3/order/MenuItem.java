9ac293fb41e97b772463bc000e892c13f1fbb475
Kimhyeyun <cqqwer@gmail.com>
2022-12-30 19:06:37 +0900

    Feat: 메뉴판 객체 생성

---
+++ src/main/java/org/example/ch3/order/MenuItem.java
@@ -0,0 +1,11 @@
+package org.example.ch3.order;
+
+public class MenuItem {
+    private final String name;
+    private final int price;
+
+    public MenuItem(String name, int price) {
+        this.name = name;
+        this.price = price;
+    }
+}
