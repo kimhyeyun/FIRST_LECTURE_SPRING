080f48daff58438ba53ff81e042898a27ec42325
Kimhyeyun <cqqwer@gmail.com>
2022-12-30 18:49:27 +0900

    Style: 오타 수정

---
+++ src/main/java/org/example/ch3/GradeCalculator.java
@@ -0,0 +1,24 @@
+package org.example.ch3;
+
+import java.util.List;
+
+public class GradeCalculator {
+    private final List<Course> courses;
+
+    public GradeCalculator(List<Course> courses) {
+        this.courses = courses;
+    }
+
+    public double calculateGrade() {
+        double multipliedCreditAndCourseGrade = 0;
+        for (Course course : courses) {
+            multipliedCreditAndCourseGrade += course.getCredit() * course.getGradeToNumber();
+        }
+
+        int totalCompletedCredit = courses.stream()
+                .mapToInt(Course::getCredit)
+                .sum();
+
+        return multipliedCreditAndCourseGrade / totalCompletedCredit;
+    }
+}
