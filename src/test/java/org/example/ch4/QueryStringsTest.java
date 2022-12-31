ff517a5ff8524e36390c1689caa9779fb152b2be
Kimhyeyun <cqqwer@gmail.com>
2022-12-31 20:01:07 +0900

    Test: HttpRequest 테스트

---
+++ src/test/java/org/example/ch4/QueryStringsTest.java
@@ -0,0 +1,16 @@
+package org.example.ch4;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class QueryStringsTest {
+    @Test
+    void createTest() {
+        QueryStrings queryStrings = new QueryStrings("operand1=11&operator=*&operand2=55");
+
+        assertThat(queryStrings).isNotNull();
+    }
+}
