ba0fdec007e1ed9fa17c766eeb060796f912469f
Kimhyeyun <cqqwer@gmail.com>
2022-12-31 19:11:22 +0900

    Feat: WAS 만들어보기

--- src/main/java/org/example/ch4/Main.java
+++ src/main/java/org/example/ch4/Main.java
@@ -1,6 +1,11 @@
 package org.example.ch4;
 
+// GET /calculate?operand1=11&&operator=*&operand2=55
+
+import java.io.IOException;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        new CustomWebApplicationServer(8080).start();
     }
 }
