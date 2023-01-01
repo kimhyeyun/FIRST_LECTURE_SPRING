361ec7702391da371fb82b87e84ba83347226bd7
Kimhyeyun <cqqwer@gmail.com>
2023-01-01 18:56:50 +0900

    Feat: Thread Pool을 적용

--- src/main/java/org/example/ch4/CustomWebApplicationServer.java
+++ src/main/java/org/example/ch4/CustomWebApplicationServer.java
@@ -10,9 +10,13 @@ import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.nio.charset.StandardCharsets;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class CustomWebApplicationServer {
     private final int port;
+
+    private final ExecutorService executorService = Executors.newFixedThreadPool(10);
 
     private static final Logger logger = LoggerFactory.getLogger(CustomWebApplicationServer.class);
 
@@ -30,7 +34,6 @@ public class CustomWebApplicationServer {
             while ((clientSocket = serverSocket.accept()) != null) {
                 logger.info("[CustomerWebApplicationServer] client  connected.");
 
-                new Thread(new ClientRequestHandler(clientSocket)).start();
 
                 /*
                 * Step 1 - 사용자 요청을 메인 Thread가 처리하도록 한다.
@@ -57,6 +60,20 @@ public class CustomWebApplicationServer {
                         response.responseBody(body);
                     }
                 }*/
+
+                /*
+                 * Step 2 - 사용자 요청이 들어올 때마다 Thread를 새로 생성해서 사용자 요청을 처리하도록 한다.
+                 * 이렇게 하면 문제 ? -> 요청이 들어올 때마다 스레드를 새로 생성하기 때문에 문제 발생
+                 * 스레드는 독립적인 스택 메모리 공간을 할당 받음 -> 성능이 매우 떨어짐
+                 * 최악의 경우 -> 서버의 리소스가 감당 불가 -> 서버 다운 가능성 존재
+                 * */
+//                new Thread(new ClientRequestHandler(clientSocket)).start();
+
+                /*
+                * Step 3 - Thread Pool을 적용해 안정적인 서비스가 가능하도록 한다.
+                * */
+
+                executorService.execute(new ClientRequestHandler(clientSocket));
             }
         }
     }
